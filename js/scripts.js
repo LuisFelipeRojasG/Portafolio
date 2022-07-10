215e7479d6060d4a151b95b0eebc2dcdcde72f28
LuisFelipeRojas <luisfeliperojas27@yahoo.es>
2022-07-09 22:13:09 -0500

    Se agrega botón de menu animado y sección stack

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+document.querySelector(".body--header-bars_menu").addEventListener("click", animateBars);
+
+var barsMenuLine1 = document.querySelector(".bars_menu-line1");
+var barsMenuLine2 = document.querySelector(".bars_menu-line2");
+var barsMenuLine3 = document.querySelector(".bars_menu-line3");
+
+function animateBars() {
+    barsMenuLine1.classList.toggle("active_bars_menu-line1");
+    barsMenuLine2.classList.toggle("active_bars_menu-line2");
+    barsMenuLine3.classList.toggle("active_bars_menu-line3");
+}
+
