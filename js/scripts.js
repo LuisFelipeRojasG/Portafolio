4b576786dcab2df444df26ba3827a6defac3cd82
LuisFelipeRojas <luisfeliperojas27@yahoo.es>
2022-09-11 18:17:17 -0500

    Menu responsive y footer contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 const menu = document.querySelector(".body--header-bars_menu");
+
 menu.addEventListener("click", animateBars);
 
 var barsMenuLine1 = document.querySelector(".bars_menu-line1");
 var barsMenuLine2 = document.querySelector(".bars_menu-line2");
 var barsMenuLine3 = document.querySelector(".bars_menu-line3");
+var menuNav = document.getElementsByClassName("body--header-bars_links")[0];
 
 function animateBars() {
     barsMenuLine1.classList.toggle("active_bars_menu-line1");
     barsMenuLine2.classList.toggle("active_bars_menu-line2");
     barsMenuLine3.classList.toggle("active_bars_menu-line3");
+
+    menuNav.classList.toggle("active_nav");
+
 }
 
 
